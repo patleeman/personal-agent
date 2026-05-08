7bb1f661c986e754f5bcca73e3023b43743c01d0
Patrick Lee <me@patricklee.nyc>
2026-05-08 16:27:24 -0400

    chore: expose knowledge host package seam

--- packages/extensions/src/host.ts
+++ packages/extensions/src/host.ts
@@ -1,4 +1,5 @@
 export * as data from './data.js';
+export * as knowledge from './knowledge.js';
 export * as settings from './settings.js';
 export * as ui from './ui.js';
 export * as workbench from './workbench.js';
